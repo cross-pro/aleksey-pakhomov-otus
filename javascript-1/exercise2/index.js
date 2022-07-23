4b5aa338764c517d28137ef9f5e3d760473060df
cross <cross22150@gmail.com>
2022-07-23 22:00:20 +0300

    укоротил еще

--- javascript-1/exercise2/index.js
+++ javascript-1/exercise2/index.js
@@ -1,11 +1,7 @@
 const sum = function() {
     return function summator(argument, lastValue = 0) {
-        if (!argument) {
-            return lastValue
-        } else {
-            return (nextParam) => {
-                return summator(nextParam, lastValue + argument)
-            }
+        return (!argument) ? lastValue : (nextParam) => {
+            return summator(nextParam, lastValue + argument)
         }
     }
 }()

70ef0d919e5645becd8368636e148053aedc061d
cross <cross22150@gmail.com>
2022-07-23 22:17:36 +0300

    еще подрезал

--- javascript-1/exercise2/index.js
+++ javascript-1/exercise2/index.js
@@ -1,7 +1,5 @@
 const sum = function() {
     return function summator(argument, lastValue = 0) {
-        return (!argument) ? lastValue : (nextParam) => {
-            return summator(nextParam, lastValue + argument)
-        }
+        return (!argument) ? lastValue : (nextParam) => summator(nextParam, lastValue + argument)
     }
 }()

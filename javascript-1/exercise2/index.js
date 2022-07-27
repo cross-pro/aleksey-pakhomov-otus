b4f810d8b2155e596746912fefd41efb0a24e118
cross <cross22150@gmail.com>
2022-07-27 18:23:07 +0300

    поправил условие на undefined

--- javascript-1/exercise2/index.js
+++ javascript-1/exercise2/index.js
@@ -1,5 +1,5 @@
 const sum = function() {
     return function summator(argument, lastValue = 0) {
-        return (!argument) ? lastValue : (nextParam) => summator(nextParam, lastValue + argument)
+        return (argument == undefined) ? lastValue : (nextParam) => summator(nextParam, lastValue + argument)
     }
 }()

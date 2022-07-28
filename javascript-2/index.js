2508c06020109655c05e4e52cff632c503b316a0
cross <cross22150@gmail.com>
2022-07-29 00:34:48 +0300

    первая версия

--- javascript-2/index.js
+++ javascript-2/index.js
@@ -1,3 +1,8 @@
 function promiseReduce(asyncFunctions, reduce, initialValue) {
-
+    return Promise.resolve(
+        Promise.all([
+            fn1().then(new Promise((p) => reduce(p, initialValue))),
+            fn2().then((param) => reduce(param, initialValue))
+        ])
+    ).then(p => p.pop())
 }

c22ade5c6c7764448d84dde1a1cb234e33b7e60a
cross <cross22150@gmail.com>
2022-11-02 21:38:04 +0300

    рефакторинг

--- javascript-3/index.js
+++ javascript-3/index.js
@@ -1,3 +1,5 @@
+import {parseElement, buildSelector, getId} from "./selector-util.js"
+
 export async function getPath(element) {
     if (element === null || element === undefined)
         return null
@@ -22,74 +24,3 @@ export async function getPath(element) {
 
     return buildSelector(path)
 }
-
-function buildSelector(path) {
-    let result = ""
-    let copyArray = [...path]
-    copyArray.reverse().map(p => {
-        let newValue = p + " > "
-        result = result + newValue
-    })
-    return result.slice(0, result.length - 3)
-}
-
-function parseElement(element, path) {
-    console.log("parse:", element.tagName)
-    if (element === null || element.tagName.toLowerCase() === "body") {
-        return null
-    }
-
-    let id = getId(element)
-    let className = getClass(element)
-    let tag = element.tagName.toLowerCase()
-
-    let result = ""
-    if (id !== null) {
-        result = id
-    } else {
-        if (className !== null) {
-            result = tag + "." + className
-        }
-        else {
-            result = tag
-        }
-    }
-
-    let uniqueSelector = getUniqueSelector(element, result)
-    path.push(uniqueSelector)
-
-    return element.parentNode
-}
-
-function getId(element) {
-    if (element.hasAttribute("id")) {
-        const id = element.getAttribute("id")
-        return "#" + id;
-    } else {
-        return null
-    }
-}
-
-function getClass(element) {
-    let elementClass
-    if (element.hasAttribute("class")) {
-        elementClass = element.getAttribute("class")
-        return elementClass.replaceAll(" ", ".")
-    } else
-        return null
-}
-
-/*если внутри блока несколько элементов возвращает позицию*/
-function getPosition(element) {
-    let index = Array.from(element.parentNode.children).indexOf(element)
-    return `${element.tagName.toLowerCase()}:nth-child(${parseInt(index) + 1})`
-}
-
-function getUniqueSelector(element, selector) {
-    if (element.parentElement.querySelectorAll(selector).length !== 1) {
-        return getPosition(element)
-    } else {
-        return selector
-    }
-}
-

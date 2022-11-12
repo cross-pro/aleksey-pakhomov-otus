dd5ffc2ecc970456af7e557a057b0628476c30f2
cross <cross22150@gmail.com>
2022-11-12 23:51:01 +0300

    + babel/ts

--- javascript-7/files-tree/webpack.config.babel.js
+++ javascript-7/files-tree/webpack.config.babel.js
@@ -62,7 +62,7 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env"],
+            presets: ["@babel/preset-env", "@babel/preset-typescript"],
           },
         },
       },

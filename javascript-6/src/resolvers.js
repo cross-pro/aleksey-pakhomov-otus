3651d0b172c3310ba38e042f7641434c6f99aa34
cross <cross22150@gmail.com>
2022-11-21 13:11:25 +0300

    пример мутации

--- javascript-6/src/resolvers.js
+++ javascript-6/src/resolvers.js
@@ -7,6 +7,39 @@ const resolvers = {
     users: () => users,
     findUserByName: (parent, { name }, context, info) => findElement("name", name, users),
   },
+  Mutation: {
+    createUser: (parent, {
+      id, name, email, age,
+    }, context, info) => {
+      const newUser = {
+        id, name, email, age,
+      }
+
+      users.push(newUser)
+
+      return newUser
+    },
+    updateUser: (parent, {
+      id, name, email, age,
+    }, context, info) => {
+      const newUser = users.find((user) => user.id === id)
+
+      newUser.name = name
+      newUser.email = email
+      newUser.age = age
+
+      return newUser
+    },
+    deleteUser: (parent, { id }, context, info) => {
+      const userIndex = users.findIndex((user) => user.id === id)
+
+      if (userIndex === -1) throw new Error("User not found.")
+
+      const deletedUsers = users.splice(userIndex, 1)
+
+      return deletedUsers[0]
+    },
+  },
 }
 
 export default resolvers

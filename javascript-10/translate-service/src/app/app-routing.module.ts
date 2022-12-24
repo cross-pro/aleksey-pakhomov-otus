e3fdcd3428a5a11649f20d55af9b8632c07eb5c8
cross <cross22150@gmail.com>
2022-12-24 17:53:08 +0300

    добавлен роутер

--- javascript-10/translate-service/src/app/app-routing.module.ts
+++ javascript-10/translate-service/src/app/app-routing.module.ts
@@ -1,12 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {RecentlyAddedComponent} from "./components/recently-added/recently-added.component";
+import {GoComponent} from "./components/go/go.component";
+import {SettingsComponent} from "./components/settings/settings.component"
 
 
+const routes: Routes = [
+  {path: "", component: RecentlyAddedComponent},
+  {path:"go", component: GoComponent},
+  {path: "settings", component: SettingsComponent}
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ], exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+
+
+
+export class AppRoutingModule {
+}

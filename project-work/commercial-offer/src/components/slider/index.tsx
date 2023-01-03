64e75a184c9d565b154501d9f03ff6ab6a7033fb
cross <cross22150@gmail.com>
2023-01-03 16:00:49 +0300

    структура слайда

--- project-work/commercial-offer/src/components/slider/index.tsx
+++ project-work/commercial-offer/src/components/slider/index.tsx
@@ -1,8 +1,22 @@
 import React from "react"
+import {SlideImage} from "../slide-image/index";
+import {SlideTitle} from "../title/index";
+import {SlideDescription} from "../description/index";
+import "./index.css"
 
 const Slider = () => {
     return (
-        <div>Slider</div>
+        <div className="slider">
+            <SlideTitle title="Косплей ведьмака"/>
+            <SlideImage
+                src={"https://webpulse.imgsmail.ru/imgpreview?mb=webpulse&key=pulse_cabinet-image-8f994035-fce9-4ec4-aa7f-134b39feb806"}
+                alt={"картинка не найдена"}/>
+            <SlideDescription description="Серия игр “Ведьмак” не была бы так хороша без Цириллы.
+            Она появляется в третьей части и сразу же вызывает симпатию.
+            Однако, книжная Цири могла бы понравиться геймерам не настолько сильно.
+             Создатели игр хорошо поработали над этим персонажем, перечислим главные изменения."/>
+
+        </div>
     )
 }
 

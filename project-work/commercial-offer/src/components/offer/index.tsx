998a428392be36a85fff13024997891d31b1a574
cross <cross22150@gmail.com>
2023-01-04 01:42:02 +0300

    дополнительные тестовые данные

--- project-work/commercial-offer/src/components/offer/index.tsx
+++ project-work/commercial-offer/src/components/offer/index.tsx
@@ -28,9 +28,19 @@ const loadSlides = (): Array<JSX.Element> => {
              Создатели игр хорошо поработали над этим персонажем, перечислим главные изменения."
                alt="Изображение не найдено"
         />)
-    slides.push(<Slide title={"Косплей цири2"}
+    slides.push(<Slide title={"Косплей цири 2"}
                        imageUrl="https://webpulse.imgsmail.ru/imgpreview?mb=webpulse&key=pulse_cabinet-image-8c79c8a8-a65d-49b1-97af-ac89842b58f9"
                        description="В игре Цирилла уверенная, ее больше не смущает шрам на лице, хотя он стал гораздо заметнее, чем в каноне. Она не переживает о своей внешности, носит открытую одежду и даже не думает себя с кем-то сравнивать."
+                       alt="Изображение не найдено"
+    />)
+    slides.push(<Slide title={"Косплей цири 3"}
+                       imageUrl="https://webpulse.imgsmail.ru/imgpreview?mb=webpulse&key=pulse_cabinet-image-1ff82314-a524-49bf-8b4c-83dd84cd3a4f"
+                       description="В книгах Анджея Сапковского Цирилла — неуверенная юная девушка с огромным количеством комплексов. Она очень стеснялась своего шрама и стремилась прикрыть его волосами. Цири постоянно сравнивала себя с другими и сильно завидовала."
+                       alt="Изображение не найдено"
+    />)
+    slides.push(<Slide title={"Косплей цири 4"}
+                       imageUrl="https://webpulse.imgsmail.ru/imgpreview?mb=webpulse&key=pulse_cabinet-image-56b79f2d-7d2b-4c3d-86ac-073a0b5d7dda"
+                       description="Особенно сильно девушка из книг завидовала красотке Йеннифэр, которая заботилась о ней. Цири просто не могла слышать, как кто-то делает Йен очередной комплимент."
                        alt="Изображение не найдено"
     />)
 

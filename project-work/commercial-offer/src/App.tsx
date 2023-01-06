597eb71329df20be30425b4e2e9500e9ac55afce
cross <cross22150@gmail.com>
2023-01-06 05:50:35 +0300

    макет формы авторизации

--- project-work/commercial-offer/src/App.tsx
+++ project-work/commercial-offer/src/App.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 import "./style/bootstrap.min.css"
+import {Login} from "./components/login/index";
 
 function App() {
 
-  return (
-    <div className="content">
-      <h1>Главная страница</h1>
-    </div>
-  )
+    let [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    if (!isLoggedIn) {
+        return (
+            <Login setIsLoggedIn={setIsLoggedIn}/>
+        )
+    } else {
+        return (
+            <div className="content">
+                <h1>Главная страница</h1>
+            </div>
+        )
+    }
 }
 
 export default App
--- project-work/commercial-offer/src/components/login/index.tsx
+++ project-work/commercial-offer/src/components/login/index.tsx
@@ -1,9 +1,62 @@
-import React from "react"
+import React, {Dispatch, SetStateAction, useEffect, useState} from "react"
+import "./style.css"
+import {ErrorMessage} from "../error-message/index";
 
-const Login = () => {
+export const Login = ({setIsLoggedIn}: { setIsLoggedIn: Dispatch<SetStateAction<boolean>> }) => {
+
+    useEffect(() => {
+
+    }, [])
+
+    let [login, setLogin] = useState("")
+    let [password, setPassword] = useState("")
+    let [errorMessage, setErrorMessage] = useState("")
+
+    const auth = () => {
+
+        console.log(login, password)
+
+        if (login === "login" && password === "password") {
+            setIsLoggedIn(true)
+        }
+        else {
+            setErrorMessage("Ошибка авторизации")
+            setLogin("")
+            setPassword("")
+
+            setTimeout(() => {
+                setErrorMessage("")
+            },2000)
+        }
+    }
+
+    const onLogin = (e: any) => {
+        setErrorMessage("")
+        setLogin(e.target.value)
+    }
+
+    const onPassword = (e: any) => {
+        setErrorMessage("")
+        setPassword(e.target.value)
+    }
+
     return (
-        <div className="login-page">
-            Login page
+        <div>
+            {errorMessage ? <ErrorMessage errorMessage={errorMessage}/> : null }
+            <div className="login-page">
+                <div className="login-form" onSubmit={auth}>
+                    {/*<form>*/}
+                        <input type="text" placeholder="Логин" className="form-control" value={login}
+                               onChange={onLogin}/>
+                        <input type="password" placeholder="Пароль" className="form-control" value={password}
+                               onChange={onPassword}/>
+                        <button className="btn btn-primary"
+                                onClick={auth}
+                        >Войти
+                        </button>
+                    {/*</form>*/}
+                </div>
+            </div>
         </div>
     )
 }

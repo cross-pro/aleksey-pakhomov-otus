2d8e2feaecd5ebf0bc466d914768be62a8d24295
cross <cross22150@gmail.com>
2023-01-12 18:44:22 +0300

    обновление слайда

--- project-work/commercial-offer/src/components/slide-view/index.tsx
+++ project-work/commercial-offer/src/components/slide-view/index.tsx
@@ -1,11 +1,61 @@
-import React from "react"
+import React, {useState} from "react"
 import "./index.css"
 import ISlide from "../../models/slide";
+import {useMutation, gql} from '@apollo/client';
 
 export const SlideView = ({slide}: { slide: ISlide }) => {
+    const UPDATE_SLIDE = gql`
+        mutation updateSlide(
+            $_id: String!,
+            $title: String!,
+            $imageUrl: String!,
+            $description: String,
+        )
+        {
+            updateSlide(_id: $_id,
+                title: $title,
+                imageUrl: $imageUrl,
+                description: $description
+            )
+            {
+                _id,
+                title,
+                imageUrl,
+                description
+            }
+        }
+    `;
+
+    let [title, setTitle] = useState(slide.title)
+    let [description, setDescription] = useState(slide.description)
+    let [imageUrl, setImageUrl] = useState(slide.imageUrl)
+
+
+    const changeTitle = (e: any) => {
+        setTitle(e.target.value)
+    }
+
+    const changeDescription = (e: any) => {
+        setDescription(e.target.value)
+    }
+
+    const changeImage = (e: any) => {
+        setImageUrl(e.target.value)
+    }
+
+    const [updateSlide] = useMutation(UPDATE_SLIDE, {
+        variables: {
+            _id: slide._id,
+            title: title,
+            description: description,
+            imageUrl: imageUrl
+        }
+    })
+
+
 
     const saveForm = () => {
-        console.log("save:", slide)
+        updateSlide()
     }
 
     return (
@@ -13,20 +63,20 @@ export const SlideView = ({slide}: { slide: ISlide }) => {
             <label htmlFor={"title" + slide._id} className="col-sm-2 control-label">Заголовок</label>
             <div className="col-sm-10">
                 <input id={"title" + slide._id} type="text" className="form-control" placeholder="Введите заголовок"
-                       defaultValue={slide.title} required/>
+                       value={title} required onChange={changeTitle}/>
             </div>
 
             <label htmlFor={"imageUrl" + slide._id} className="col-sm-2 control-label">Изображение</label>
             <div className="col-sm-10">
                 <input id={"imageUrl" + slide._id} type="text" className="form-control" placeholder="Изображение"
-                       defaultValue={slide.imageUrl} required/>
+                       value={imageUrl} required onChange={changeImage}/>
             </div>
 
-            <label htmlFor={"description" + slide._id} className="col-sm-2 control-label">Изображение</label>
+            <label htmlFor={"description" + slide._id} className="col-sm-2 control-label">Описание</label>
             <div className="col-sm-10">
 
                 <input id={"description" + slide._id} type="text" className="form-control" placeholder="Описание"
-                       defaultValue={slide.description} required/>
+                       value={description} required onChange={changeDescription}/>
             </div>
 
             <div className="col-sm-10">

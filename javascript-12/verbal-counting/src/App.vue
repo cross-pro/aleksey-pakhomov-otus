1b52a4e0b7a6139cb6c471f041dfc5d9a208ebe9
cross <cross22150@gmail.com>
2023-01-15 12:20:41 +0300

    пакет роутера

--- javascript-12/verbal-counting/src/App.vue
+++ javascript-12/verbal-counting/src/App.vue
@@ -2,13 +2,19 @@
     import Settings from "./components/settings/Settings.vue"
     import Game from "./components/game/Game.vue"
 
+    const routes = [
+    { path: '/', component: Settings },
+    { path: '/game', component: Game },
+]
+
+
 </script>
+
 
 <template>
     <Navigation/>
     <div class="content">
-        <Settings/>
-       <!-- <Game/>-->
+        <!-- <Settings/> -->
+       <Game/>
     </div>
-    <!--<HelloWorld msg="Vite + Vue"/>-->
 </template>

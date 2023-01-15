69908044c2ec20fdf730aae2fef15286f738dd56
cross <cross22150@gmail.com>
2023-01-15 13:10:37 +0300

    макет кнопок

--- javascript-12/verbal-counting/src/components/game/Game.vue
+++ javascript-12/verbal-counting/src/components/game/Game.vue
@@ -1,9 +1,36 @@
 <script setup lang="ts">
-    
+
 
 
 </script>
 
+<style>
+  @import './style.css';
+</style>
+
 <template>
-    Game page
+    <div class="game">
+        <div class="number-line">
+            <input class="number-button" type="button" value="1" />
+            <input class="number-button" type="button" value="2" />
+            <input class="number-button" type="button" value="3" />
+            <input class="action-button" type="button" value="<" />
+        </div>
+        <div class="number-line">
+            <input class="number-button" type="button" value="4" />
+            <input class="number-button" type="button" value="5" />
+            <input class="number-button" type="button" value="6" />
+            <input class="action-button" type="button" value=">" />
+        </div>
+        <div class="number-line">
+            <input class="number-button" type="button" value="7" />
+            <input class="number-button" type="button" value="8" />
+            <input class="number-button" type="button" value="9" />
+            <input class="action-button" type="button" value="?" />
+        </div>
+        <div class="number-line">
+            <input class="number-button" type="button" value="0" />
+            <input class="action-button" type="button" value="=" />
+        </div>
+    </div>
 </template>
